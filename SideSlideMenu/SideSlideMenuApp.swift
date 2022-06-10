efc90489b30365c6085e4ed42fc542528c73b083
songjie <ijaysdev@gmail.com>
2022-06-10 10:40:25 +0800

    Initial Commit

---
+++ SideSlideMenu/SideSlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideSlideMenuApp.swift
+//  SideSlideMenu
+//
+//  Created by Jay Song on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct SideSlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
